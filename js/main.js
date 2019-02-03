99213b80d00acdfae2e77fb54b0733134da01223
Your Name <jinathin@gmail.com>
2019-02-03 13:50:04 -0500

    adding some basics and read me update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//create jar, name jar, place string in jar 
+var firstName = "Marco";
+
+//alert is method(built in function in JS)
+//alert("Hello" + firstName);
+
+//modern JS 
+//alert ('Hello ${firstName}');
+
+//console.log("Hello" + " " + firstName)
+
+console.log("javascript linked up");

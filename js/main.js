5e75ef0cc73e8eba4bb86ad2e6a3aa26858bc9a0
Aaron Paterson <juniha.aaron@gmail.com>
2019-03-13 15:04:58 -0400

    updated queries

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ const allowDrop = (e) => {
     e.preventDefault();
 };
 
-
 //  When the music dropped start playing only the sounds YOUR DRAGGING AWESOME!!!!!
 const dropSound = (e) => {
     e.preventDefault();
@@ -115,38 +114,6 @@ const startDrag = (e) => {
         document.querySelector('.bgModal').style.display = "none";
     });
 
-
-
-const dropSound = (e) => {
-    e.preventDefault();
-    alert('Booya!');
-};
-
-
-(() => {
-
-
-	console.log("javascript linked up");
-	// below does the samething.
-	// document.querySelectorAll("#musicCon > div, #melodyCon > div").forEach((circle) => {
-	// Make all music circles dragable
-    document.querySelectorAll(".musicMedia, .melodyMedia").forEach((circle) => {
-        circle.setAttribute('draggable', true);
-    });
-
-	// // Setup an array for the music mixer ES6 JS 
-	// let theMusic =[]
-
-	// // get the music from each section
-	// const musicCon = document.querySelector("#musicCon");
-	// const melodyCon = document.querySelector("#melodyCon");
-
-	const dragPanel = document.querySelector('.dragMedia');
-
-    dragPanel.addEventListener('dragover', allowDrop);
-    dragPanel.addEventListener('drop', dropSound);
-
-	// setup play on 'drop' then add more music on top of the first drop
 
 
 })(); 
